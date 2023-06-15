f044be627f3afdee62d1a4d74d31bd4e577065ea
vansh-saeculum <vansh.panchal@saeculumsolutions.com>
2023-06-15 18:59:18 +0530

    15/06

---
+++ 15-06-23/index.js
@@ -0,0 +1,31 @@
+var num = 20;
+console.log(num);
+
+var num1;
+console.log(num1);
+
+
+       {
+            var num=10;
+        }
+        console.log(num);
+
+
+        function abc() {
+            var num = 10;
+        }
+        console.log(num)
+
+
+        console.log(num);
+        {
+            var num = 10;
+        }
+
+
+        var abc=10;
+        console.log(abc);
+
+
+        let const=10;
+        console.log(const);
