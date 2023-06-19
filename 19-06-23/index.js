037724036e71ccafe60e86ae947a1e58e7991786
vansh-saeculum <vansh.panchal@saeculumsolutions.com>
2023-06-19 19:06:49 +0530

    19/06

---
+++ 19-06-23/index.js
@@ -0,0 +1,86 @@
+// let obj = {
+//     "first name": "Vansh",
+//     "last name": "Panchal"
+// }
+// console.log(obj["first name"] + " " + obj["last name"]);
+
+// let arr = [(uni_name = ["Silver Oak University"]), "branch", "CE"];
+// console.log(object[0][0], object[2]);
+// console.log(object[object.length - 1]);
+// console.log(typeof arr);
+// console.log(Array.isArray(arr));
+// console.log(arr instanceof String);
+
+// let main_obj = {
+//     "_full name" : "Vansh Panchal"
+// }
+// console.log(main_obj["_full name"]);
+
+// let _mainobj = {
+//   "_full-name": "Vansh Panchal",
+// };
+// console.log(_mainobj["_full-name"]);
+
+// let $mainobj = {
+//   "_full+name": "Vansh Panchal",
+// };
+// console.log($mainobj["_full+name"]);
+
+// let mainobj = {
+//   "$full/name": "Vansh Panchal",
+// };
+// console.log(mainobj["$full/name"]);
+
+// "use strict";
+// let fname = {
+//   num1: 5,
+//   num2: 10,
+//   num3: 15,
+// };
+// let num0 = ["num1", "num2", "num3"];
+// console.log(fname[num0[0]]);
+
+// let fullname = {
+//   fname: "Vanshh",
+//   lname: "Panchal",
+// };
+// console.log(fullname.fname + " " + fullname["lname"]);
+
+// let user1 = {
+//   name: "vansh",
+//   sname: {
+//     first: "panchal",
+//   },
+// };
+
+// let user2 = Object.assign({}, user1);
+// let user2 = { ...user1 };
+// let user2 = JSON.parse(JSON.stringify(user1));
+
+// user2.sname.first = "patel";
+
+// console.log(user1.sname);
+// console.log(user2.sname);
+
+// operators
+
+// arithmetic operators
+
+let a = 20;
+let b = 10;
+
+// console.log(a + b); //addition
+// console.log(a - b); //substraction
+// console.log(a * b); //multiplication
+// console.log(a / b); //division
+// console.log(a % b); //remainder
+// console.log(++a); //pre-increment
+// console.log(a++); //post-increment
+// console.log(b--); //pre-decrement
+// console.log(--b); //post-decrement
+// console.log(b ** 2); //exponential
+
+let c = 15;
+console.log(a + b / c);
+console.log(a + b * c);
+console.log((a * b) / c);
