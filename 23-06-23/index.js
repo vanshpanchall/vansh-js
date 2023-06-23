689c7f832704ec24c8a562cb8c45454c3831bb0c
vansh-saeculum <vansh.panchal@saeculumsolutions.com>
2023-06-23 19:37:32 +0530

    23-06

---
+++ 23-06-23/index.js
@@ -0,0 +1,129 @@
+//STRINGS
+
+//using single inverted commas
+// let str = "Hello there";
+
+//using double inverted commas
+let str = "Hello there!";
+
+//using backtick
+let str1 = ` How are you?`;
+
+// console.log(str, str1;
+
+//string length
+// console.log(str.length);
+
+//accessing characters
+// console.log(str[0]);
+
+//concatenating the strings
+// console.log(str + " " + str1);
+
+//converting values to strings
+
+//string() constructor
+// let num = 45;
+// let numStr = String(num);
+// console.log(typeof numStr);
+
+// //toString() method
+// let numtoStr = num.toString();
+// console.log(typeof numtoStr);
+
+// //string concatenation
+// let concNum = "" + num;
+// console.log(typeof concNum);
+
+//string comparison
+
+// console.log("hi" == "hi");
+// console.log("hi" === "hi");
+// console.log("hi" != "hii");
+// console.log("hi" !== "hii");
+// console.log("a" > "b");
+// console.log("a" >= "b");
+// console.log("a" < "b");
+// console.log("a" < 1);
+// console.log("a" <= "b");
+
+//string methods
+
+let smtg = "Hello there! My name is Vansh!";
+
+//string.at()
+console.log(smtg.at(-6));
+
+//string.charAt()
+console.log(smtg.charAt(0));
+// console.log(smtg.charAt(-1)); //can't add negative value
+
+//string.charCodeAt()
+console.log(smtg.charCodeAt(0));
+
+//concat
+let newStr = str.concat(str1);
+console.log(newStr);
+
+//String.endsWith()
+console.log(smtg.endsWith("Vansh!"));
+
+//String.startsWith()
+console.log(smtg.startsWith("Hello"));
+
+//includes
+console.log(smtg.includes("My"));
+
+// index.of
+console.log(smtg.indexOf("h"));
+
+//lastindexof()
+console.log(smtg.lastIndexOf("h"));
+
+//padStart
+console.log(smtg.padStart(35, "."));
+
+//padEnd
+console.log(smtg.padEnd(35, "."));
+
+//replace
+console.log(smtg.replace("Vansh", "Bhaumik"));
+
+//replaceAll
+console.log(smtg.replaceAll("!", "."));
+
+//search
+console.log(smtg.search("Vansh"));
+
+// object inside the function
+// function greet() {
+//   let obj1 = {
+//     a: 10,
+//     b: 20,
+//   };
+//   return obj1;
+// }
+// console.log(greet());
+
+// let a = "hello";
+// let b = a.slice(-2, -1);
+// console.log(b);
+
+// let abc = { a: 10, b: 15 };
+// var a = 15;
+// console.log(a);
+// let { a: d, b } = abc;
+// console.log(a);
+
+// let obj1 = {
+//   a: 10,
+//   b: 20,
+//   c: {
+//     d: 30,
+//     e: {
+//       f: 40,
+//       g: 50,
+//     },
+//   },
+// };
+// console.log(obj1);
---
+++ 23-06-23/stringQnA.js
@@ -0,0 +1,203 @@
+// 1
+// let str = "Hello! there ";
+// console.log(str.length);
+
+// 2
+// console.log(str.toUpperCase());
+
+// 3
+// console.log(str.indexOf("o"));
+
+// 4
+// console.log("Hii" + " " + "Hello");
+// let newStr = str.concat(str);
+// console.log(newStr);
+
+// 5
+// console.log(str.slice(0, 6));
+// console.log(str.substring(0, 6));
+
+// 6
+// console.log(str.replace("Hello", "Hii"));
+
+// 7
+// console.log(str.split("!"));
+
+// 8
+// let str1="     Hii! This is white space    "
+// console.log(str1.trimStart());
+// console.log(str1.trimEnd());
+
+// 9
+// console.log(str.search("there"));
+
+// 10
+// console.log(str.startsWith("Hello!"));
+
+// 11
+// const message = "Hello, World!";
+// console.log(message.length);
+
+// 12
+// const str = "HelloWorld";
+// console.log(str.toLowerCase());
+
+// 13
+// const sentence = "I love JavaScript!";
+// console.log(sentence.indexOf("JavaScript"));
+
+// 14
+// const name = "John";
+// const greeting = `Hello, ${name}!`;
+// console.log(greeting);
+
+// 15
+// const text = "Hello, World!";
+// console.log(text.substring(7));
+
+// 16
+// const message = "Hello, World!";
+// console.log(message.replace("World", "Universe"));
+
+// 17
+// const words = "apple, banana, orange";
+// const fruits = words.split(", ");
+// console.log(fruits);
+
+// 18
+// const text = "   Hello, World!   ";
+// console.log(text.trim());
+
+// 19
+// const sentence = "The quick brown fox jumps over the lazy dog.";
+// console.log(sentence.includes("dog"));
+
+// 20
+// const str = "Hello, World!";
+// console.log(str.endsWith("!"));
+
+// 21
+// const sentence = "JavaScript is fun!";
+// console.log(sentence.charAt(4));
+
+// 22
+// const str1 = "Hello";
+// const str2 = "World";
+// console.log(str1.concat(", ", str2));
+
+// 23
+// const sentence = "JavaScript is awesome!";
+// console.log(sentence.slice(0, 10));
+
+// 24
+// const text = "Hello, World!";
+// console.log(text.repeat(3));
+
+// 25
+// const sentence = "The quick brown fox";
+// console.log(sentence.split(" "));
+
+// 26
+// const str = "Hello, World!";
+// console.log(str.indexOf("o"));
+
+// 27
+// const sentence = "JavaScript is fun!";
+// console.log(sentence.substring(4, 10));
+
+// 28
+// const text = "Hello, World!";
+// console.log(text.charAt(7));
+
+// 29
+// const sentence = "I love JavaScript!";
+// console.log(sentence.split("o"));
+
+// 30
+// const str = "Hello, World!";
+// console.log(str.includes("World"));
+
+// 31
+// const sentence = "JavaScript is cool!";
+// console.log(sentence.lastIndexOf("o"));
+
+// 32
+// const text = "Hello, World!";
+// console.log(text.startsWith("Hello"));
+
+// 33
+// const str = "Hello, World!";
+// console.log(str.toUpperCase());
+
+// 34
+// const sentence = "JavaScript is awesome!";
+// console.log(sentence.toLowerCase());
+
+// 35
+// const str = "Hello, World!";
+// console.log(str.charAt(13));
+
+// 36
+// const sentence = "JavaScript is fun!";
+// console.log(sentence.substr(4, 10));
+
+// 37
+// const text = "Hello, World!";
+// console.log(text.includes("WORLD"));
+
+// 38
+// const sentence = "JavaScript is amazing!";
+// console.log(sentence.replace("amazing", "awesome"));
+
+// 39
+// const str = "Hello, World!";
+// console.log(str.charCodeAt(4));
+
+// 40
+//  const sentence = "JavaScript is powerful!";
+//  console.log(sentence.split(" ").length);
+
+// 41
+//  const text = "Hello, World!";
+//  console.log(text.search("W"));
+
+// 42
+// const sentence = "JavaScript is dynamic!";
+// console.log(sentence.slice(-7));
+
+// 43
+// const str = "Hello, World!";
+// console.log(str.concat(" Goodbye!"));
+
+// 44
+// const sentence = "JavaScript is flexible!";
+// console.log(sentence.charAt(sentence.length - 1));
+
+// 45
+// const sentence = "JavaScript is interesting!";
+// console.log(sentence.substring(10));
+
+// 46
+// const str = "Hello, World!";
+// console.log(str.search("World"));
+
+// 47
+// const sentence = "JavaScript is versatile!";
+// console.log(sentence.split(" ").reverse().join(" "));
+
+// 48
+// const sentence = "JavaScript is powerful!";
+// console.log(sentence.substring(4, 10));
+
+// 49
+// const str = "Hello, World!";
+// console.log(str.charCodeAt(0));
+
+// 50
+// const sentence = "JavaScript is amazing!";
+// console.log(sentence.slice(4, 10));
+
+// 51
+ const text = "Hello, World!";
+ console.log(text.indexOf("o", 5));
+
