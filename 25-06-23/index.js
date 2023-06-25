03ecdc6b546f6904d72004f6ca086828762a51b3
vansh-saeculum <vansh.panchal@saeculumsolutions.com>
2023-06-26 01:12:33 +0530

    25-06

---
+++ 25-06-23/index.js
@@ -0,0 +1,76 @@
+// let a = 10.000000000000001;
+// if (a > 10) {
+//   console.log("The number is Greater than 10");
+// }
+
+// let num = 4;
+// if (num % 2 === 0) {
+//   console.log("Even number");
+// }
+
+// let a = 1;
+// if (a > 10) {
+//   console.log("The number is Greater than 10");
+// } else {
+//   console.log("The number is Less than 10");
+// }
+
+// let age = 17;
+// if (age >= 18) {
+//   console.log("You are eligible to vote");
+// } else {
+//   console.log("You are not eligible to vote");
+// }
+
+// let number = 5;
+// if (number % 2 === 0) {
+//   console.log(number + " is even");
+// } else {
+//   console.log(number + " is odd");
+// }
+
+// let age = 18;
+// if (age > 19) {
+//   console.log("You are an adult");
+// } else if (age >= 13) {
+//   console.log("You are a teenager");
+// } else {
+//   console.log("You are a kiddo");
+// }
+
+// let age = 6;
+// if (age > 23) {
+//   console.log("You're a work proffessional");
+// } else if (age >= 19) {
+//   console.log("You're in College");
+// } else if (age >= 17) {
+//   console.log("You're in Higher Secondary School");
+// } else if (age >= 8) {
+//   console.log("You're in Primary School");
+// } else if (age >= 5) {
+//   console.log("You're in Kindergarten");
+// } else {
+//   console.log("You can't do Schooling");
+// }
+
+// let city = "Ahmedabad";
+// if (city == "Ahmedabad") {
+//   console.log("You're from Gujarat");
+// } else if (city == "Mumbai") {
+//   console.log("You're from Maharashtra");
+// } else {
+//   console.log("You're an alien");
+// }
+
+let age = 19;
+switch (age) {
+  case 18:
+    console.log("You are 18");
+    break;
+  case 17:
+    console.log("You are 17");
+    break;
+  default:
+    console.log("Your age is undefined");
+    break;
+}
