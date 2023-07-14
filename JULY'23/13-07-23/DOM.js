21bfd51ab8149d1b5b230b979e91abcc2f6fbae8
vansh-saeculum <vansh.panchal@saeculumsolutions.com>
2023-07-14 12:29:52 +0530

    Modified Month Wise

--- JULY'23/13-07-23/DOM.js
+++ JULY'23/13-07-23/DOM.js
@@ -91,18 +91,3 @@ box4.classList.add("border");
 // console.log(box1.getAttributeNS("height"));
 // console.log(box1.getAttributeNames("height"));
 
-// let firstEle = document.body;
-// const newHeading = document.createElement("h1");
-// newHeading.textContent=("Hello There");
-// firstEle.appendChild(newHeading)
-// firstEle.append(newHeading)
-
-let newElement = document.getElementById("box-1");
-newH2 = document.createElement("h2");
-newH2.textContent = "This is DOM!";
-newElement.appendChild(newH2);
-
-document.write("This is Document Write");
-
-let range = document.createRange();
-console.log(range);
---
+++ JULY'23/14-07-23/script.js
@@ -0,0 +1,15 @@
+// let firstEle = document.body;
+// const newHeading = document.createElement("h1");
+// newHeading.textContent=("Hello There");
+// firstEle.appendChild(newHeading)
+// firstEle.append(newHeading)
+
+let newElement = document.getElementById("box-1");
+newH2 = document.createElement("h2");
+newH2.textContent = "This is DOM!";
+newElement.appendChild(newH2);
+
+document.write("This is Document Write");
+
+let range = document.createRange();
+console.log(range);
