9a00042bab45b66857e5ff0eaab2f506ed02f7b8
vansh-saeculum <vansh.panchal@saeculumsolutions.com>
2023-07-17 14:07:44 +0530

    before lunch

--- JULY'23/17-07-23/Task 1/script.js
+++ JULY'23/17-07-23/Task 1/script.js
@@ -1,4 +1,20 @@
-let onCLick = document.getElementsByClassName("clickEvent");
-onCLick.addEventListener("click", (e) => { 
-    e.style.color = 'red';
-})
+// let onCLick = document.querySelectorAll(" .sidenav");
+// onCLick.addEventListener("click", function() {
+// //    return e.style.color = 'red';
+//     onCLick.alert("clicked");
+// });
+
+// function changeColor() {
+//     // document.querySelectorAll(".clickEvent").style.color = 'red';
+//     document.querySelectorAll(".clickEvent");
+//     classList.add('active');
+// }
+
+
+function openNav() {
+  document.getElementById("slideNav").style.width = "100%";
+}
+
+function closeNav() {
+  document.getElementById("slideNav").style.width = "0";
+}
