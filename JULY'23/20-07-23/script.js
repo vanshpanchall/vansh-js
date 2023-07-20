362c19617bec24e94d346be7e3eb465ed53046de
vansh-saeculum <vansh.panchal@saeculumsolutions.com>
2023-07-20 19:26:01 +0530

    20-07

--- JULY'23/20-07-23/script.js
+++ JULY'23/20-07-23/script.js
@@ -7,6 +7,7 @@ var z = 0;
 // KEYUP
 document.addEventListener("keyup", (e) => {
   let msg = (document.querySelector("#up span").textContent = x += 1);
+  // console.log(e);
   //   msg.textContent = alert("This is Keyup");
   //   console.log('This is Keyup');
 });
@@ -14,13 +15,15 @@ document.addEventListener("keyup", (e) => {
 // KEYDOWN
 document.addEventListener("keydown", (e) => {
   let msg = (document.querySelector("#down span").textContent = y += 1);
+  // console.log(e);
   //     msg.textContent = alert("This is keydown");
   //   console.log("This is keydown");
 });
 
 // KEYPRESS
-document.addEventListener("keydown", (e) => {
+document.addEventListener("keypress", (e) => {
   let msg = (document.querySelector("#press span").textContent = z += 1);
+  // console.log(e);
   //   msg.textContent = alert("This is keypress");
   //   console.log("This is keypress");
 });
@@ -31,20 +34,22 @@ var a = 0;
 var b = 0;
 var c = 0;
 var d = 0;
-var e = 0;
+var i = 0;
 var f = 0;
 var h = 0;
 
 // MOUSE CLICK
-click.addEventListener("click", () => {
-  let msg = (document.querySelector("#click span").textContent = e += 1);
+click.addEventListener("click", (e) => {
+  let msg = (document.querySelector("#click span").textContent = i += 1);
+  // console.log(e);
   // msg.textContent = alert('The Mouse CLicked');
   //   console.log("The Mouse CLicked");
 });
 
 // MOUSE DOUBLE CLICK
-dblclick.addEventListener("dblclick", () => {
+dblclick.addEventListener("dblclick", (e) => {
   let msg = (document.querySelector("#dblclick span").textContent = f += 1);
+  // console.log(e);
   // msg.textContent = alert('The Mouse CLicked');
   //   console.log("The Mouse Double CLicked");
 });
@@ -52,30 +57,35 @@ dblclick.addEventListener("dblclick", () => {
 // MOUSE ENTER
 enter.addEventListener("mouseenter", (e) => {
   let msg = (document.querySelector("#enter span").textContent = a += 1);
+  // console.log(e);
   //   msg.textContent = alert('The Mouse Entered');
 });
 
 // MOUSE OVER
 over.addEventListener("mouseover", (e) => {
   let msg = (document.querySelector("#over span").textContent = b += 1);
+  // console.log(e);
   // msg.textContent = alert("The Mouse overed");
 });
 
 // MOUSE MOVE
 move.addEventListener("mousemove", (e) => {
   let msg = (document.querySelector("#move span").textContent = c += 1);
+  // console.log(e);
   //   msg.textContent = alert("The Mouse Moved");
 });
 
 // MOUSE LEAVE
 leave.addEventListener("mouseleave", (e) => {
   let msg = (document.querySelector("#leave span").textContent = d += 1);
+  // console.log(e);
   // msg.textContent = alert("The Mouse Left");
 });
 
 // MOUSE OUT
 out.addEventListener("mouseout", (e) => {
   let msg = (document.querySelector("#out span").textContent = h += 1);
+  //   console.log(e);
   // msg.textContent = alert("The Mouse Out");
 });
 
@@ -83,10 +93,11 @@ out.addEventListener("mouseout", (e) => {
 
 const body = document.body;
 const div = document.createElement("div");
-// div.innerText='This div is created using JS!'
-// div.textContent = "This div is created again using JS!";
-div.innerHTML = "<h1>This div is created again using JS!</h1>";
-body.append(div);
+div.innerText = "This div is created using JS!";
+div.textContent = "This div is created again using JS!";
+// div.innerHTML = "<h1>This div is created again using JS!</h1>";
+// body.append(div);
+body.appendChild(div);
 
 // ADDING NEW CLASS
 div.classList.add("font");
@@ -121,6 +132,7 @@ divSample.append(spanRemove);
 // spanRemove.dataset.newName = "Bhaumik";
 
 // CHECKING WHETHER THE CLICKED TARGET IS DIV OR NOT
+
 // const divs = document.querySelectorAll("div");
 // document.addEventListener("click", (e) => {
 //   if (e.target.matches("div")) {
@@ -130,11 +142,21 @@ divSample.append(spanRemove);
 //   }
 // });
 
-let bodyXY = document.querySelector("#position");
-document.addEventListener("mousemove", (e) => {
-    let a = e.screenX;
-    let b = e.screenY;
-    let c = e.clientX;
-    let d = e.clientY;
-    bodyXY.innerText = a + b + c + d;
-});
+// GETTING X/Y POSITION OF MOUSE
+
+// let bodyXY = document.querySelector("#position");
+// document.addEventListener("mousemove", (e) => {
+//   let a = e.screenX;
+//   let b = e.screenY;
+//   let c = e.clientX;
+//   let d = e.clientY;
+//   bodyXY.innerText = a + b + c + d;
+// });
+
+// GETTING THE KEY WHICH WE PRESS
+
+// document.addEventListener("keydown", e => {
+//     document.querySelector("#key").textContent =
+// e.key;
+// console.log(e.key);
+// });
