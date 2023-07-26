f1887714165d8cd0f8516468a1e00ac190fd631d
vansh-saeculum <vansh.panchal@saeculumsolutions.com>
2023-07-26 19:31:07 +0530

    26-07

--- JULY'23/26-07-23/Task 1/script.js
+++ JULY'23/26-07-23/Task 1/script.js
@@ -9,18 +9,54 @@ btn.addEventListener("click", (e) => {
   let a = document.createElement("li");
   if (input.length >= 3) {
     a.innerHTML = input;
-    let b = confirm("Are You Sure?");
+    // let b = confirm("Are You Sure?");
 
-    if (b) {
-      addTask.appendChild(a);
-    }
+    // if (b) {
+    addTask.appendChild(a);
+    // }
   }
   task.value = "";
 
+  let newDiv = document.createElement("div");
+
+  let check = document.createElement("span");
+  check.innerHTML = `<i class="fa-solid fa-check"></i>`;
+  check.style.marginRight = "25px";
+  check.style.cursor = "pointer";
+  check.style.color = "lightgreen";
+
+  newDiv.appendChild(check);
+
   let span = document.createElement("span");
   span.innerHTML = `<i class="fa-solid fa-xmark"></i>`;
+  //   span.innerHTML = `<i class="fa-solid fa-trash"></i>`;
+  //   span.style.fontSize = "15px";
   span.style.cursor = "pointer";
-  a.appendChild(span);
+  span.style.color = "tomato";
+  newDiv.appendChild(span);
+  a.appendChild(newDiv);
+
+  check.addEventListener("click", (e) => {
+    a.style.textDecoration = "line-through";
+    a.style.textDecorationColor = "grey";
+    a.style.textDecorationThickness = "2px";
+    a.style.color = "grey";
+
+    let undo = document.createElement("span");
+    undo.innerHTML = `<i class="fa-solid fa-rotate-left"></i>`;
+    undo.style.position = "absolute";
+    undo.style.top = "29%";
+    undo.style.right = "-4%";
+    undo.style.color = "yellow";
+    undo.style.cursor = "pointer";
+    a.appendChild(undo);
+
+    undo.addEventListener("click", (e) => {
+      a.style.color = "yellow";
+      a.style.textDecoration = "none";
+      undo.remove();
+    });
+  });
 
   span.addEventListener("click", (e) => {
     a.remove();
