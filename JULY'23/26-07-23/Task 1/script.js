8065514097df886fa6ea30f590a05100a974d559
vansh-saeculum <vansh.panchal@saeculumsolutions.com>
2023-07-27 13:25:40 +0530

    before lunch

--- JULY'23/26-07-23/Task 1/script.js
+++ JULY'23/26-07-23/Task 1/script.js
@@ -1,23 +1,25 @@
 const btn = document.querySelector(".btn");
 
-btn.addEventListener("click", (e) => {
-  const task = document.querySelector(".task");
-  const addTask = document.querySelector(".add-task");
+const task = document.querySelector(".task");
+const addTask = document.querySelector(".add-task");
 
+btn.addEventListener("click", (e) => {
   let input = task.value.trim();
 
   let a = document.createElement("li");
   if (input.length >= 3) {
-    a.innerHTML = input;
-    // let b = confirm("Are You Sure?");
+    a.textContent = input;
+    let b = confirm("Are You Sure?");
 
-    // if (b) {
-    addTask.appendChild(a);
-    // }
+    if (b) {
+      addTask.appendChild(a);
+    }
   }
   task.value = "";
 
   let newDiv = document.createElement("div");
+
+  // ADDING DONE BUTTON
 
   let check = document.createElement("span");
   check.innerHTML = `<i class="fa-solid fa-check"></i>`;
@@ -26,6 +28,8 @@ btn.addEventListener("click", (e) => {
   check.style.color = "lightgreen";
 
   newDiv.appendChild(check);
+
+  // ADDING DELETE BUTTON
 
   let span = document.createElement("span");
   span.innerHTML = `<i class="fa-solid fa-xmark"></i>`;
@@ -36,27 +40,52 @@ btn.addEventListener("click", (e) => {
   newDiv.appendChild(span);
   a.appendChild(newDiv);
 
-  check.addEventListener("click", (e) => {
-    a.style.textDecoration = "line-through";
-    a.style.textDecorationColor = "grey";
-    a.style.textDecorationThickness = "2px";
-    a.style.color = "grey";
+  // DONE FUNCTIONALITY
 
-    let undo = document.createElement("span");
-    undo.innerHTML = `<i class="fa-solid fa-rotate-left"></i>`;
-    undo.style.position = "absolute";
-    undo.style.top = "16px";
-    undo.style.right = "-35px";
-    undo.style.color = "yellow";
-    undo.style.cursor = "pointer";
-    a.appendChild(undo);
+  check.addEventListener("click", () => {
+    a.classList.toggle("text");
 
-    undo.addEventListener("click", (e) => {
-      a.style.color = "yellow";
-      a.style.textDecoration = "none";
-      undo.remove();
-    });
+    // ON CLICK REMOVE AND ADD
+
+    // if ((a.style.textDecoration = "line-through")) {
+    //   a.style.color = "yellow";
+    //   a.style.textDecoration = "none";
+    // } else {
+    //   a.style.textDecoration = "line-through";
+    //   a.style.textDecorationColor = "grey";
+    //   a.style.textDecorationThickness = "2px";
+    //   a.style.color = "grey";
+    // }
+
+    // FOR UNDO BUTTON
+
+    // let undo = document.createElement("span");
+    // undo.innerHTML = `<i class="fa-solid fa-rotate-left"></i>`;
+    // undo.style.position = "absolute";
+    // undo.style.top = "16px";
+    // undo.style.right = "-35px";
+    // undo.style.color = "yellow";
+    // undo.style.cursor = "pointer";
+    // a.appendChild(undo);
+
+    // undo.addEventListener("click", (e) => {
+    //   a.style.color = "yellow";
+    //   a.style.textDecoration = "none";
+    //   undo.remove();
+    // });
   });
+
+  // ADDING EDIT FUNCTIONALITY
+
+  const edit = document.createElement("span");
+  // edit.innerHTML = `<i class="fa-solid fa-pen"></i>`;
+  edit.innerHTML = `<i class="fa-regular fa-pen-to-square"></i>`;
+  edit.classList.add("edit");
+  a.appendChild(edit);
+
+
+
+  // DELETING FUNCTIONALITY
 
   span.addEventListener("click", (e) => {
     a.remove();
