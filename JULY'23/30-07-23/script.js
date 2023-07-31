4e165146dc520d188efa0e6a82c6ea21ae21c78b
vansh-saeculum <vansh.panchal@saeculumsolutions.com>
2023-07-31 11:37:21 +0530

    before coffee break

---
+++ JULY'23/30-07-23/script.js
@@ -0,0 +1,33 @@
+// console.log(typeof NaN);
+
+// console.log(99999999999999999);
+
+// console.log(0.5 + 0.1 == 0.6);
+
+// console.log(0.2 + 0.1 == 0.3);
+
+// console.log(Math.max());
+
+// console.log(Math.min());
+
+// console.log([] + []);
+
+// console.log([] + {});
+
+// console.log({} + []);
+
+// console.log(true + true + true == 3);
+
+// console.log(true - true);
+
+// console.log(true == 1);
+
+// console.log(true === 1);
+
+// console.log((!+[] + [] + ![]).length);
+
+// console.log(9 + "1");
+
+// console.log(91 - "1");
+
+// console.log([]==0);
---
+++ JULY'23/31-07-23/script.js
@@ -0,0 +1,11 @@
+let input = document.querySelector("#addTask");
+let submit = document.querySelector(".add-btn");
+let tasks = document.querySelector(".todo-tasks");
+
+submit.addEventListener("click", (e) => {
+  let tr = document.createElement("tr");
+  tr.innerHTML = `<td>${input.value}</td>`;
+  tasks.appendChild(tr);
+  e.preventDefault();
+  input.value = "";
+});
