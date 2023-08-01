85c66062b121551ede550b31b8be2d9bc32bf322
vansh-saeculum <vansh.panchal@saeculumsolutions.com>
2023-08-01 16:31:10 +0530

    before coffee break

---
+++ AUG'23/01-08-23/CRUD/script.js
@@ -0,0 +1,76 @@
+const srNum = document.querySelector("#num");
+const studName = document.querySelector("#name");
+const rollNo = document.querySelector("#roll");
+const branch = document.querySelector("#branch");
+
+const add = document.querySelector(".add");
+const table = document.querySelector(".new-tr");
+
+let store = null;
+
+function addTask(e) {
+  if (srNum.value.trim() != "") {
+    if (store) {
+      update(store);
+      //   store = null;
+      //   alert("Enter below Fields");
+    } else {
+      const newTr = document.createElement("tr");
+      newTr.classList.add("taskTr");
+      newTr.innerHTML = `
+    <td>${srNum.value}</td>
+    <td>${studName.value}</td>
+    <td>${rollNo.value}</td>
+    <td>${branch.value}</td>
+    `;
+
+      const edit = document.createElement("span");
+      edit.innerHTML = `<i onclick="editTask(this)" class="fa-solid fa-pen-to-square"></i>`;
+      edit.classList.add("editBtn");
+      newTr.appendChild(edit);
+
+      const deleteRow = document.createElement("span");
+      deleteRow.innerHTML = `<i onclick="deleteTask(this)" class="fa-solid fa-xmark"></i>`;
+      deleteRow.classList.add("delBtn");
+      newTr.appendChild(deleteRow);
+
+      table.appendChild(newTr);
+    }
+    reset();
+  }
+}
+
+function editTask(e) {
+  let a = e.parentElement.parentElement;
+  store = a;
+  // console.log(a);
+  // console.log(e);
+  // console.log(store);
+  // console.log(a.children[0].innerText);
+  srNum.value = a.children[0].innerText;
+  studName.value = a.children[1].innerText;
+  rollNo.value = a.children[2].innerText;
+  branch.value = a.children[3].innerText;
+}
+
+function update(e) {
+  e.children[0].textContent = srNum.value;
+  e.children[1].textContent = studName.value;
+  e.children[2].textContent = rollNo.value;
+  e.children[3].textContent = branch.value;
+}
+
+function deleteTask(e) {
+  let confirm = confirm("Are You Sure?");
+  if (confirm) {
+    let targetRow = e.parentElement.parentElement;
+    targetRow.remove();
+  }
+}
+
+function reset() {
+  srNum.value = "";
+  studName.value = "";
+  rollNo.value = "";
+  branch.value = "";
+}
